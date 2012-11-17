6a2cf91f10dcd93d8f96097c5635877d55658f98
Brian Martin <bmarti44@students.kennesaw.edu>
2012-11-17 17:13:15 -0500

    Working on npm install

--- sites/default/settings.js
+++ sites/default/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'base': '/Users/bmarti44/Documents/Aptana Studio 3 Workspace/Cliste',
+	'base': process.cwd(),
 	'theme': 'bootstrap',
 	'domain': 'clistecms.com',
 	'port': 8888
