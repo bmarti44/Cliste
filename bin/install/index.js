6e5f0f038ed17b69a5cad192171dc102d0dea92a
Brian Martin <bmarti44@students.kennesaw.edu>
2012-11-17 14:31:27 -0500

    Working on npm install

--- bin/install/index.js
+++ bin/install/index.js
@@ -6,23 +6,6 @@ var sys = require('sys'),
 	install,
 	link;
 
-terminal.stdout.on('data', function (data) {
-    console.log('stdout: ' + data);
-});
+link = exec("git clone https://github.com/bmarti44/Cliste.git");
 
-install = exec("npm install require-dir handlebars mongodb mongoose node-static -g", function (error, stdout, stderr) {
-	if (error === null) {
-		link = exec("npm link require-dir");
-		console.log("Module require-dir installed!");
-		link = exec("npm link handlebars");
-		console.log("Module handlebars installed!");
-		link = exec("npm link mongodb");
-		console.log("Module mongodb installed!");
-		link = exec("npm link mongoose");
-		console.log("Module mongoose installed!");
-		link = exec("npm link node-static");
-		console.log("Module node-static installed!");
-	} else {
-		console.log('cliste install error: ' + error);
-	}
-});
+process.exit(0);
